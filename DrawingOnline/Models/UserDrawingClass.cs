31b353708d0842fa0445421524e9c4199da3a8c2
ismager7878 <62778246+ismager7878@users.noreply.github.com>
2022-01-25 03:12:33 +0100

    coollll!!!!

---
+++ DrawingOnline/Models/UserDrawingClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrawingOnline.Models
+{
+    public class UserDrawingClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Canvas { get; set; }
+        public bool Remixed { get; set; }
+        public Nullable<int> RemixedID { get; set; }
+        public Nullable<int> UserID { get; set; }
+        public bool Publicity { get; set; }
+
+        public string CanvasSVG { get; set; }
+    }
+}
